b8a714b001c546ca3c540267e84fa2a01a101689
EdenRavecca <edenravecca@u.boisestate.edu>
2022-02-15 09:37:02 -0700

    cleaning and tracks scripts

---
+++ cleaning_PRFA.R
@@ -0,0 +1,270 @@
+
+
+############################ DATA CLEANING SCRIPT ##############################
+#.......................    Records/GPS/Projection   ...........................
+
+
+# load packages relevant to this script:
+
+library( sp )
+library( tidyverse ) # easy data manipulation and plotting
+# set option to see all columns and more than 10 rows
+options( dplyr.width = Inf, dplyr.print_min = 100 )
+#library( amt ) # creating tracks from location data
+library(sf)
+
+##############################  Load PRFA Data  ################################
+#.................... PRFA data Shared Research Drive   ........................
+
+# Clean your workspace to reset your R environment
+
+rm( list = ls() )
+load( "cleaningPRFA21.RData" )
+
+# Set working directory. This is the path to your Rstudio folder for this 
+# project. If you are in your correct Rstudio project then it should be:
+
+getwd()
+
+# if so then:
+workdir <- getwd()
+
+# set path to where you can access your data
+
+datapath <- "Z:/Common/PrairieFalcons/"
+
+# Import GPS data 
+# Fixes are stored as separate CSV files for each individual
+# Create a function that imports multiple files at once:
+
+load_data <- function( path ){
+  # extract all file names in the folder
+  myfiles <- dir( path, pattern = '\\.csv', full.names = TRUE )
+  for( i in 1:length(myfiles) ){
+    mydata <- read.csv( file = myfiles[i], 
+                        # remove white spaces  
+                        strip.white =TRUE, 
+                        # include column headers
+                        header = TRUE, 
+                        # read the serial column as a character instead of number:
+                        colClasses = c("serial" = "character") ) 
+    # create df for first file and attach rows for other files
+    ifelse( i == 1,
+            df <- mydata, 
+            df <- bind_rows(df, mydata) ) ## I'm not sure what's going on here
+  } 
+  #return df for all individuals
+  return( df )
+}
+
+# apply function to import all files as list of databases:
+
+dataraw <- load_data( paste0(datapath, 'allindvs/') )
+# Note that the files are all in a subdirectory
+
+# Import trapping records with details of when transmitters were deployed
+
+records <- read.csv( file = paste0( datapath,"survey_0.csv" ),
+                     # replace values with NA
+                     na.strings = c(""," ","NA"), 
+                     # include column headings
+                     header = TRUE )
+# check
+head( records ); dim( records )
+
+# import polygon of the NCA as sf spatial file:
+
+NCA_Shape <- sf::st_read("Z:/Common/QCLData/Habitat/NCA/GIS_NCA_IDARNGpgsSampling/BOPNCA_Boundary.shp")
+
+
+############################ Cleaning Records ##################################
+#.........................  Survey 123 PRFA data  ..............................
+
+colnames( records )
+
+# keep transmitter id, date and sex
+
+records <- records %>% dplyr::select( Telemetry.Unit.ID, Sex, 
+                                      Date.and.Time )
+# view
+
+records
+
+# convert date to correct format using lubridate
+
+records$StartDate <- lubridate::mdy_hms( records$Date.and.Time, 
+                                         tz = "UTC")
+
+# Add a day so that we can ignore records from the trapping day (or prior)
+# and start only with those from the following day:
+
+records$StartDate <- records$StartDate + lubridate::days(1)
+
+# convert start date to Julian date
+
+records$StartDay <- lubridate::yday( records$StartDate )
+
+# unit IDs were recorded without the beginning of their serial number
+# add beginning of CTT ID number, so we can match survey123 records
+# to the GPS serial IDs:
+
+records$serial <- paste0( '894608001201', records$Telemetry.Unit.ID )
+
+# check 
+
+head( records); dim( records)
+
+############################ Cleaning GPS data ################################
+#........................       CTT PRFA data      .............................
+
+# GPS units often provide information on the quality of the fixes
+# Units from CTT provide HDOP, VDOP and `time to fix` 
+# Start by viewing what those look like in the dataset
+
+hist( dataraw$vdop, breaks = 50 )
+hist( dataraw$hdop, breaks = 50 )
+hist( dataraw$time_to_fix )
+
+# Remove 2D fixes and fixes where HDOP or VDOP ≥10 following
+# D’eon and Delparte (2005); Poessel et al. (2016).
+# Also those where time to fix > 20min or with 0 satellites:
+
+# start by creating a new dataframe to store cleaned location records:
+
+datadf <- dataraw 
+
+colnames( datadf )
+
+# Filter to remove inaccurate locations
+
+datadf <- datadf %>% dplyr::filter( hdop < 10 ) %>%
+  dplyr::filter( vdop < 10 ) %>%
+  dplyr::filter( time_to_fix <= 20 ) %>% 
+  dplyr::filter( nsats > 0 ) %>%
+  dplyr::filter( lat > 0 ) %>% 
+  # remove superfluous columns
+  dplyr::select( -inactivity, -geo, -data_voltage, -solar_current, 
+                 -solar_charge )
+
+# view
+
+head( datadf ); dim( datadf )
+
+# How many rows did we remove?
+dim( dataraw ) - dim( datadf )
+
+# We also need to create a time column containing date and time information
+# in POSIX format (as required by amt)
+# Data are stored in GPS_YYYY.MM.DD_HH.MM.SS format in our data 
+# We define correct format with lubridate 
+
+datadf$date <- lubridate::ymd_hms( datadf$GPS_YYYY.MM.DD_HH.MM.SS,
+                                   tz = "UTC" )
+
+# create new column where we convert it to posixct
+
+datadf$ts <- as.POSIXct( datadf$date )
+
+# view
+
+head( datadf ); dim( datadf ) 
+## Whats's the difference between date and ts? They look the same..
+
+# check if any data are missing
+
+all( complete.cases( datadf ) )
+# none so we can move on
+
+# add seperate month and day of year columns using lubridate
+
+datadf <- datadf %>% 
+  mutate( mth = lubridate::month(date),
+          jday = lubridate::yday(date) )
+
+# Remove records from prior to deployment 
+# Keep relevant information from the records dataframe 
+# Combine datadf to records df
+
+datadf <- records %>%  dplyr::select( serial, Sex, StartDay ) %>% 
+  right_join( datadf, by = "serial" )
+# select serial, sex, StartDay from the records df and right_join to combine
+# with datadf by `serial` ID
+
+# view
+
+head( datadf ); dim( datadf )
+
+# Using StartDay to filter records, remove records that occurred prior to 
+# StartDay (deployment)
+
+datadf <- datadf %>% 
+  group_by( serial ) %>% 
+  dplyr::filter( jday > StartDay ) %>% ungroup() 
+# why group and ungroup?
+
+# view
+
+head( datadf ); dim( datadf )
+
+# Create a new individual ID column:
+
+datadf$id <- group_indices( datadf, serial )
+
+# Using serial ID unique to each individual found in df, add territory column
+# In Territory column each serial ID is linked to its corresponding territory
+
+datadf <- datadf %>%
+  mutate(territory = case_when(
+    endsWith(serial, "47221") ~ "SG",
+    endsWith(serial, "47775") ~ "CRW",
+    endsWith(serial, "47874") ~ "SDTP",
+    endsWith(serial, "48120") ~ "PR_II",
+    endsWith(serial, "46751") ~ "HHGS_DS",
+    endsWith(serial, "46983") ~ "HHGS_US",
+    endsWith(serial, "47197") ~ "Mac",
+    endsWith(serial, "48229") ~ "CRW_new",
+    endsWith(serial, "48377") ~ "CFR",
+  ))
+
+unique(datadf$territory)
+colnames(datadf)
+
+#########################   Define CRS/Projection  #############################
+#........................       CTT PRFA data      .............................
+
+# Define coordinate system and projection for the data
+# GPS data were recorded using WGS84 in lat lon
+# Use the epsg code to define coordinate system
+# https://spatialreference.org/ref/epsg/wgs-84/ 
+# epgs = 4326 for this coordinate system
+
+# For amt, crs need to be provided using sp package # why sp package?
+
+# crsdata <- sp::CRS( "+init=epsg:4326" ) # old script, this line no longer works, deprecated
+crsdata <- 4326 # sp::CRS( "+init=epsg:4326" ) # updated
+
+# Transform the lat lon to eastings and northings using UTM
+# http://www.dmap.co.uk/utmworld.htm
+# zone 11:
+
+# crstracks <- sp::CRS( "+proj=utm +zone=11" )
+crstracks <- sf::st_crs( NCA_Shape )
+
+# Convert the NCA shapefile to the same projection as GPS data and for the 
+# tracks that will be created in next script
+
+NCA_Shape <- sf::st_transform( NCA_Shape, crstracks ) # this line doesn't make sense with the previous line
+# seems not needed
+
+# Next Script: Defining Tracks
+
+save.image( "cleaningPRFA21.RData" )
+
+#########################    end of script  ####################################
+
+
+
+
+
+
+
---
+++ tracks_PRFA.R
@@ -0,0 +1,394 @@
+
+
+
+############################# CREATING TRACKS ##################################
+#.......................  For HR using AMT package  ............................
+
+
+#..................................................................#
+# Script developed by Jen Cruz to calculate home ranges            # 
+# We rely on amt vignette here:                                    #
+# https://cran.r-project.org/web/packages/amt/vignettes/p2_hr.html #
+# as well as: Signer & Fieberg (2021) PeerJ9:e11031                #
+# http://doi.org/10.7717/peerj.11031                               #
+#..................................................................#
+
+########################## DEFINING TRACKS SCRIPT ##############################
+#.......................    For HR Estimation AMT  .............................
+
+
+# load packages relevant to this script:
+
+library( tidyverse ) #easy data manipulation
+library( amt )
+library( sp )
+library( lubridate ) #easy date manipulation
+library( sf )
+
+# Load or create data
+# Clean your workspace to reset your R environment. #
+rm( list = ls() )
+
+# Set working directory. This is the path to your Rstudio folder for this 
+# project. If you are in your correct Rstudio project then it should be:
+# getwd()
+# if so then:
+# workdir <- getwd()
+
+# load workspace 
+load( "cleaningPRFA21.RData" )
+
+# We are now ready to make tracks using atm package
+# We first check sample size
+
+table( datadf$id )
+
+# How many individuals have we dropped so far?
+# 1
+
+# We can also get an idea of the data collection for each individual
+# by plotting histograms
+
+# sampling dates
+
+ggplot( datadf, aes( x = jday, group = territory ) ) +
+  theme_classic( base_size = 15 ) +
+  geom_histogram( ) +
+  facet_wrap( ~ territory )
+
+# speeds traveled (in knots)
+
+ggplot( datadf, aes( x = speed, group = territory ) ) +
+  theme_classic( base_size = 15 ) +
+  geom_histogram( ) +
+  facet_wrap( ~ territory )
+
+# What do the histograms tell you about the nature of the data?
+# Sample size, intensity for different individuals?
+# Answer:
+
+# Why is the first bar on the speed histograms so tall?
+# Answer: spent a lot of time resting or perched
+
+# Do we need to remove data based on these?
+# Answer:
+
+#............................................................................#
+######   Creating tracks, calculating step lengths and turning angles   ######
+#.....             for all individuals at the same time:                .....#
+#............................................................................#
+
+# amt requires us to turn data into tracks for further analyses.
+
+trks <- datadf %>% 
+  # make track. Note you can add additional columns to it.
+  amt::make_track(.y = lat, .x = lon, .t = ts, 
+                  #define columns that you want to keep, relabel if you need:
+                  id = id, sex = Sex, mth = mth,jday = jday, speed = speed, 
+                  alt = alt, territory = territory, 
+                  # assign correct crs
+                  crs = crsdata )
+
+# Reproject to UTM to convert lat lon to easting northing:
+
+trks <- amt::transform_coords( trks, crs_to = crstracks )
+
+# Turn into a tibble list by grouping and nest by individual IDs:
+
+trks <- trks %>%  amt::nest( data = -"territory" )
+
+# view
+
+trks
+
+# Remember we have multiple types of data including detailed data for flights:
+# 3 times a week, 30min fixes during the day, then hourly fixes during migration.
+# We start by focusing on data during breeding season.
+# That means we need to remove migration locations.
+# How do we know when individuals started migrating North?
+# Plot overall paths for each individual:
+
+for( i in 1:dim(trks)[1]){
+  a <- as_sf_points( trks$data[[i]] ) %>% 
+    ggplot(.) + theme_bw(base_size = 17) +
+    labs( title = paste0('individual =', trks$territory[i]) ) +
+    geom_sf(data = NCA_Shape, inherit.aes = FALSE ) +
+    geom_sf() 
+  print(a)
+}
+
+# Which ones have migration paths?
+# Answer: they all have points outside the NCA
+#
+# Any ideas on how to remove migration data?
+# Answer: bounding box of NCA
+# 
+# Here we rely on NCA polygon, removing records that exist East of the NCA.
+# We can extract the extent of a polygon:
+
+sf::st_bbox(NCA_Shape)
+
+# Then use the Eastern & Northern coordinates to filter out data 
+
+xmax <- as.numeric(st_bbox(NCA_Shape)$xmax) # 627081.5
+ymax <- as.numeric(sf::st_bbox(NCA_Shape)$ymax)  + 10000
+
+# subset those tracks less than as breeding and those > as migrating:
+
+trks <- trks %>% mutate(
+  breeding = map( data, ~ filter(., x_ < xmax, y_ < ymax ) ),
+  migrating = map( data, ~ filter(., x_ >= xmax, y_ < ymax ) ) )
+
+# view
+
+trks
+
+# Use info to report how many usable points we have during time of each inv @ NCA.
+# Note we created two other groups of tibbles for the breeding season and migrating
+# Plot step lengths
+
+for( i in 1:dim(trks)[1]){
+  a <-  steps( trks$breeding[[i]] ) %>% 
+    # a <-  steps( trks$migrating[[i]] ) %>% 
+    mutate( jday = lubridate::yday( t1_ ) ) %>% #what is t1_?
+    group_by( jday ) %>% # daily
+    summarise( sl_ = sum(sl_)/1000 ) %>% # sl = step lengths or distance traveled 
+    ggplot(.) + theme_bw(base_size = 17) +
+    geom_line( aes( y = sl_, x = jday), color = "dark green", lwd = 0.75) +
+    ylim(NA,400) + 
+    labs( title = paste0('individual =', trks$territory[i]),
+          y = "Daily distance (km)", x = "Day of the year" )
+  print(a)
+}
+
+# Focus on breeding season data:
+# Estimate sampling rate for each individual by looping through data using 
+# purrr function map( )
+
+sumtrks <- trks %>%  summarize( 
+  map( breeding, amt::summarize_sampling_rate ) )
+
+# view
+
+sumtrks[[1]] # sampling rate is seconds between fixes
+
+# Add tibbles with added step lengths calculated by bursts from breeding season data:
+
+trks.all <- trks %>% mutate(
+  steps = map( breeding, function(x) 
+    x %>%  track_resample( rate = seconds(5), 
+                           tolerance = seconds(5)) %>% 
+      steps_by_burst() ) )
+
+# view
+
+trks.all
+
+
+# plot autocorrelation for step lengths for all individuals
+
+par( mfrow = c( 3,3 ) ) # what's this?
+
+for( i in 1:dim(trks.all)[1] ){
+  # extract individual ids
+  idd <- trks.all$territory[i]
+  # extract step lengths for each individual
+  x <- pull( trks.all[["steps"]][[i]], sl_) # direction_p )
+  # remove missing data
+  x <- x[!is.na(x)]
+  # calculate autocorrelation function based on step length
+  acf( x, lag.max = 600,
+       main = paste0( "individual = ",idd ) )
+  # Note you can modify the lag.max according to your data # lag is in minutes?
+}
+
+# What is ACF ?
+# Autocorrelation Function
+# What would be a reasonable rate to resample at?
+# Answer:
+
+# Choose 30min
+
+trks.all <- trks.all %>% 
+  mutate(red = map(breeding, function(x ) x %>%  
+                     track_resample( rate = minutes(30),
+                                     tolerance = minutes(5) ) ) )
+# view
+trks.all
+
+# difference between rate and tolerance?????????????????????????????????????????
+
+# plot autocorrelation for turning angles? for all individuals
+
+for( i in 1:dim(trks.all)[1] ){
+  # extract individual ids
+  idd <- trks.all$territory[i]
+  # extract step lengths for each individual
+  x <- pull( trks.all[["steps"]][[i]], direction_p )
+  # remove missing data
+  x <- x[!is.na(x)]
+  # calculate autocorrelation function based on step length
+  acf( x, lag.max = 600,
+       main = paste0( "individual = ",idd ) )
+  # Note you can modify the lag.max according to your data # lag is in minutes?
+}
+
+# I keep the original sampling rate (all autocorrelation as is) and  
+# also resample the 'breeding' tibbles to 30 min intervals
+
+trks.all <- trks.all %>% 
+  mutate(red = map(breeding, function( x ) x %>%  
+                     track_resample( rate = minutes(30),
+                                     tolerance = minutes(5) ) ),
+         red.steps = map( breeding, function(x) 
+           x %>%  track_resample( rate = seconds(30), 
+                                  tolerance = seconds(30)) %>% 
+             steps_by_burst() ) ) # plot autocorrelation for step lengths for all individuals
+# Lost here....................................................................?
+
+# view
+
+trks.all
+
+# We can now unnest the dataframes of interest
+# Starting with all breeding season data
+
+trks.breed <- trks.all %>% select( territory, breeding ) %>% 
+  unnest( cols = breeding ) 
+
+head( trks.breed )
+
+# the step dataframes re-sampled at 5 sec intervals 
+
+trks.steps <- trks.all %>% select( territory, steps ) %>% 
+  unnest( cols = steps ) 
+
+head( trks.steps )
+
+# Now breeding season data, without autocorrelation:
+
+trks.thin <- trks.all %>% select( territory, red ) %>% 
+  unnest( cols = red ) 
+
+head( trks.thin )
+
+# Now breeding season data, without autocorrelation:
+
+trks.steps30 <- trks.all %>% select( territory, red.steps ) %>% 
+  unnest( cols = red.steps ) 
+
+head( trks.steps30 )
+
+# Last all migration data:
+
+trks.mig <- trks.all %>% select( territory, migrating ) %>% 
+  unnest( cols = migrating ) 
+
+head( trks.mig )
+
+########## step lengths and turning angles
+
+# We can plot step lengths by:
+# trks.steps30 %>% 
+
+trks.steps %>%   
+  ggplot(.) +
+  # geom_density( aes( x = sl_, fill = as.factor(burst_)), alpha = 0.4 ) +
+  geom_histogram( aes( x = sl_ ) ) +
+  xlab("Step length" ) + 
+  # ylim( 0, 0.01 ) + xlim(0, 2000 ) +
+  theme_bw( base_size = 19 )  +
+  theme( legend.position = "none" ) +
+  facet_wrap( ~territory, scales = 'free_y' )
+
+# What does the plot tell us about the step lengths traveled by the individual?
+# Answer:
+
+# Turning angles:
+
+trks.steps %>% #filter( id == 1 ) %>% 
+  ggplot( .) +
+  geom_histogram( aes( x = ta_ ) ) +
+  #geom_histogram( aes( x = direction_p ) ) +
+  #coord_polar() +
+  ylab("Turning angle") + xlab("") + 
+  theme_bw( base_size = 19 ) +
+  facet_wrap( ~territory, scales = 'free_y' )
+# I think x lab is supposed to be turning angle and ylab count?
+
+# Is there any evidence of biased movements for this individual?
+# Answer: The 
+
+#..............................................................................#
+# Saving relevant objects and data
+
+# save breeding season data (not thinned)
+
+write_rds( trks.breed, "trks.breed")
+
+#save breeding season data (turned into steps)
+
+write_rds( trks.steps, "trks.steps" )
+
+#save breeding season data (thinned)
+
+write_rds( trks.thin, "trks.thin" )
+
+#save breeding season data (steps thinned)
+
+write_rds( trks.steps30, "trks.steps30" )
+
+#save migration data (unthinned)
+
+write_rds( trks.mig, "trks.mig" )
+
+#save workspace in case we need to make changes
+
+save.image( "TracksWorkspace.RData" )
+
+
+###################               END OF SCRIPT       ##########################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
